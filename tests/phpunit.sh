dcaf14eb18e06f56870a1ac7158d2bf441875648
wangxianlong <wangxianlong@mararun.com>
2017-07-26 19:28:28 +0800

    fix

--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 PHP=/usr/local/php-7.0.13/bin/php
-INI=${DIR}/php.ini
 PHPUNIT=/usr/local/php/bin/phpunit
 XML=${DIR}/phpunit.xml
-COMMAND="${PHP} -c ${INI} ${PHPUNIT} --stderr --configuration ${XML}"
+COMMAND="${PHP} ${PHPUNIT} --stderr --configuration ${XML}"
 echo ${COMMAND}
 ${COMMAND}
